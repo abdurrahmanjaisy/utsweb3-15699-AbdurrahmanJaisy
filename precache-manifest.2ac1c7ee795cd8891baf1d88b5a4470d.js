8a7ff751ad3d781000b0138aa317f4a00e2aed32
Jaisy <abdurrahmanjaisy@mail.ugm.ac.id>
2020-04-15 15:13:37 +0700

    Updates

---
+++ precache-manifest.2ac1c7ee795cd8891baf1d88b5a4470d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5f9dd1ab78ed3041ac1548b1be861e",
+    "url": "/utsweb3-15699-AbdurrahmanJaisy/index.html"
+  },
+  {
+    "revision": "a805eb6ac20c48000bb5",
+    "url": "/utsweb3-15699-AbdurrahmanJaisy/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d56896997e94831252b0",
+    "url": "/utsweb3-15699-AbdurrahmanJaisy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a805eb6ac20c48000bb5",
+    "url": "/utsweb3-15699-AbdurrahmanJaisy/static/js/2.2c5c46bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/utsweb3-15699-AbdurrahmanJaisy/static/js/2.2c5c46bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d56896997e94831252b0",
+    "url": "/utsweb3-15699-AbdurrahmanJaisy/static/js/main.2aa55ec8.chunk.js"
+  },
+  {
+    "revision": "921d4c339f7c13863b98",
+    "url": "/utsweb3-15699-AbdurrahmanJaisy/static/js/runtime-main.a651499b.js"
+  }
+]);
